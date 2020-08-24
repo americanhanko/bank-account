97dc18971e2bca6720696b3534626c60ecd16826
Eric Hanko <eric.hanko@oracle.com>
2020-08-24 12:51:00 -0700

    SQL account storage adapter?

--- ports/account_storage.py
+++ ports/account_storage.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from bank_account import BankAccount
 from ports import account_storage_in_memory_adapater as adapter
+# from ports import account_storage_sql_database_adapter
 
 
 def store(account: BankAccount) -> None:
