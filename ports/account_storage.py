b9b34162dda572b2acc8d554b6d90250a31d2bf0
Eric Hanko <eric.hanko@oracle.com>
2020-08-31 12:59:36 -0700

    Failing tests with file adapter
    Co-authored-by: Daniel Olewski <dolewski@msn.com>
    Co-authored-by: Jack Busa <jbusa@umich.edu>
    Co-authored-by: Jacob Zaval <jazava@microsoft.com>
    Co-authored-by: Mike Brown <thebrownmike@me.com>
    Co-authored-by: Linda Xie <lily.essence.art@gmail.com>

--- ports/account_storage.py
+++ ports/account_storage.py
@@ -1,8 +1,8 @@
 from typing import Optional
 
 from bank_account import BankAccount
-from ports import account_storage_in_memory_adapater as adapter
-# from ports import account_storage_sql_database_adapter
+# from ports import account_storage_in_memory_adapter as adapter
+from ports import account_storage_file_adapter as adapter
 
 
 def store(account: BankAccount) -> None:
