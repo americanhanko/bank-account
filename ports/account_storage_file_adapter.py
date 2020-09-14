f1dbb88ff93a3c7abeeb0aa21a9574d36c326325
jbusa <jbusa@umich.edu>
2020-09-14 17:00:03 -0400

    Add in file adapter

--- ports/account_storage_file_adapter.py
+++ ports/account_storage_file_adapter.py
@@ -1,11 +1,22 @@
+import yaml
 from typing import Optional
 
 from bank_account import BankAccount
 
 
 def store(account: BankAccount):
-    pass
-
+    with open(file='stored_account.yml', mode='w') as file_account:
+        account_exporter = account.__dict__
+        del account_exporter['_lock']
+        yaml.dump(account_exporter, file_account)
 
 def retrieve() -> Optional[BankAccount]:
-    pass
+    try:
+        with open(file='stored_account.yml', mode='r') as file_account:
+            account_importer = yaml.load(file_account)
+            account = BankAccount()
+            account.open()
+            account._balance = account_importer['_balance']
+            return account
+    except FileNotFoundError:
+        return None
--- ports/tests/test_storing_accounts.py
+++ ports/tests/test_storing_accounts.py
@@ -1,11 +1,10 @@
 from bank_account import BankAccount
 from ports import account_storage
-
+import os
 
 def test_first_retrieval_returns_no_account():
     account = account_storage.retrieve()
     assert not account
-
 
 def test_can_retrieve_previously_opened_account():
     stored_account = BankAccount()
@@ -15,3 +14,6 @@ def test_can_retrieve_previously_opened_account():
     retrieved_account = account_storage.retrieve()
 
     assert stored_account.balance == retrieved_account.balance
+
+def teardown_module():
+    os.remove('stored_account.yml')
